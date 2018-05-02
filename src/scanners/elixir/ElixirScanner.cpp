c4aa38637bfd52c0db63807e6f63cd3cfc2b61ca
Steve Wagner <ciroque@live.com>
2018-05-02 08:26:48 -0700

    Working towards better parsing

--- src/scanners/elixir/ElixirScanner.cpp
+++ src/scanners/elixir/ElixirScanner.cpp
@@ -25,16 +25,42 @@ FileStats *ElixirScanner::handleToken(std::string token, std::string value, File
 std::string ElixirScanner::cleanValue(std::string value) {
     StringUtils::trim(value);
     unsigned long index = value.find(" do");
-
     if(index + 3 == value.length()) {
         value = value.substr(0, value.length() - 3);
     }
+
+    // TODO: Refactor this whole method, removing commas should happen
+    // AFTER the disentanglement step...
+//    index = value.find(',');
+//    if(index != std::string::npos) {
+//        value = value.substr(0, index);
+//    }
 
     return value;
 }
 
 std::vector<std::string> ElixirScanner::disentangleMultiAliases(std::string value) {
     // TODO: Handle nefarious constructs such as "alias Scooter.Sov.{FinalSpendDetail, FinalSpendsPeriod, Spend}"
-    std::vector<std::string> aliases = { value };
+
+    std::vector<std::string> aliases;
+
+    unsigned long index = value.find('{');
+    if(index != std::string::npos) {
+        std::string prefix = value.substr(0, index);
+
+        std::cout << "\r\n!! value >> " << value << "\r\n";
+
+        std::string multis = value.substr(index + 1, (value.length() - (index + 2)));
+
+        std::cout << "\r\n!! prefix >> " << prefix << "\r\n";
+        std::cout << "\r\n!! multis >> " << multis << "\r\n";
+
+        aliases.push_back(prefix);
+        aliases.push_back(multis);
+
+    } else {
+        aliases.push_back(value);
+    }
+
     return aliases;
 }
